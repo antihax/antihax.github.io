d7ae89ab7069e0ad19b26326b45902dc08cd190f
Danni <antihax@gmail.com>
2019-11-02 14:46:47 -0500

    proper link?

--- mapviewer.js
+++ mapviewer.js
@@ -48,7 +48,7 @@ class WorldMap extends React.Component {
     }).addTo(map);
 
     var extraLife = L.easyButton('<img src="180529.svg" height=63 width=63>', function (btn, map) {
-      window.location.href = 'https://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=395919';
+      window.location.href = 'https://www.extra-life.org/participant/395919';
     }, "I've signed up for #EXTRALIFE to raise money for sick and injured kids at my local children's hospital. I need YOUR help to reach my fundraising goal #ForTheKids!").addTo(map);
     extraLife.button.style.width = '75px';
     extraLife.button.style.height = '75px';
