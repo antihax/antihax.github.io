3c156b269eb0c0efe210f44525a4c1611b849286
Danni <antihax@gmail.com>
2020-10-29 16:49:52 -0500

    not responsible for broken monitors

---
+++ atlastools/closestresources.js
@@ -0,0 +1,119 @@
+'use strict';
+require('tty')
+const fs = require('fs');
+const {
+    nextTick
+} = require('process');
+
+let rawdata = fs.readFileSync('./json/islands.json');
+let islands = JSON.parse(rawdata);
+
+rawdata = fs.readFileSync('./json/resourceTypes.json');
+let types = JSON.parse(rawdata);
+
+//var home = islands[292];
+
+for (var h in islands) {
+    var home = islands[h];
+    home.types = {};
+    if (home.name.includes("Trench") || home.name.includes("PVE") || home.name.includes("Cave"))
+        continue;
+    for (var island in islands) {
+        var i = islands[island];
+        if (i.name.includes("Trench") || i.name.includes("PVE") || i.name.includes("Cave"))
+            continue;
+        var dist = distance(home.worldX, home.worldY, i.worldX, i.worldY)
+
+        for (var type in types) {
+            if (hasType(i.resources, type)) {
+                if (types[type] == "Metal" || types[type] == "Stone") {
+                    // if (types[type] != "Berry" && types[type] != "Vegetable" && types[type] != "BAD" && types[type] != "Base" && types[type] != "Herb" && types[type] != "Coal" && types[type] != "Ignore" && types[type] != "Coral") {
+                    if (!home.types[type] || home.types[type].distance > dist) {
+                        home.types[type] = {
+                            distance: dist,
+                            grid: i.grid,
+                            id: i.id,
+                            type: types[type]
+                        }
+                    }
+                }
+            }
+        }
+    }
+    var avgDist = 0,
+        count = 0;
+
+    for (var type in home.types) {
+        count++;
+        avgDist += home.types[type].distance;
+    }
+    avgDist /= count;
+    if (home.claimable === 1)
+        console.log(home.grid, home.name, home.id, avgDist)
+
+
+}
+console.log(home.types);
+
+// distance in toroidal space
+function distance(x1, y1, x2, y2) {
+
+    var dx = Math.abs(x2 - x1);
+    var dy = Math.abs(y2 - y1);
+
+    if (dx > 7700000)
+        dx = 15400000 - dx;
+
+    if (dy > 7700000)
+        dy = 15400000 - dy;
+
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+
+    await sleep(20000);
+
+
+    // Sleep in loop
+    for (let i = 0; i < 50; i++) {
+        if (i === 3)
+            await sleep(2000);
+
+    }
+}
+
+demo();
+
+function contains(list, search) {
+    for (var i in search) {
+        for (var x in list) {
+            if (x == search[i]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function isA(type, search) {
+    for (var x in search) {
+        if (x === type) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function hasType(list, type) {
+    for (var x in list) {
+        if (x.startsWith(type)) {
+            return true;
+        }
+    }
+    return false;
+}
--- atlastools/lookforthings.js
+++ atlastools/lookforthings.js
@@ -1,24 +1,59 @@
 'use strict';
-
+require('tty')
 const fs = require('fs');
+const {
+    nextTick
+} = require('process');
 
 let rawdata = fs.readFileSync('./json/islands.json');
 let islands = JSON.parse(rawdata);
 
+rawdata = fs.readFileSync('./json/resourceTypes.json');
+let types = JSON.parse(rawdata);
+
 for (var island in islands) {
     var i = islands[island];
-    if (contains(i.resources, ["Quartz", "Herkimer", "Crystal", "Pearl", "Calcite", "Tellurite", "Amethyst"]))
+    i.types = {};
+    /*if (contains(i.resources, ["Quartz", "Herkimer", "Crystal", "Pearl", "Calcite", "Tellurite", "Amethyst"]))
         if (contains(i.resources, ["Opal", "Diamond", "Gem", "Garnet", "Ruby", "Sunstone", "Emerald"]))
             if (contains(i.resources, ["Sugar", "Sugars", "Gum", "Honey", "Saps", "Sap", "Nectar", "Sugarcane", "Syrup", "Resin"]))
                 if (contains(i.resources, ["Flake Salt", "Salt", "Pink Salt", "Iodine", "Rock Salt", "Kala Namak", "Sea Salt"]))
                     if (contains(i.resources, ["Iridium", "Tin", "Silver", "Copper", "Cobalt", "Iron"]))
                         if (contains(i.resources, ["Mineral Oil", "Crude Oil", "Shale Oil", "Naptha"]))
+                           // if (!i.animals.includes("Snake") && !i.animals.includes("GiantSnake"))*/
+    if (i.claimable !== 1)
+        continue;
 
-                           // if (!i.animals.includes("Snake") && !i.animals.includes("GiantSnake"))
-                                //if (i.claimable === 1)
-                                console.log(i.grid)
-
+    for (var type in types) {
+        if (hasType(i.resources, type))
+            if (i.types[types[type]])
+                i.types[types[type]]++;
+            else
+                i.types[types[type]] = 1;
+    }
+    var count = i.types["Wood"] + i.types["Thatch"] + i.types["Fiber"] ;
+    if (count >= 5 && (i.types["Coal"] > 0 || i.types["Oil"]) > 0)
+        console.log(count, i.grid, i.name, i.types, i.discoveries);
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+
+    await sleep(20000);
+
+
+    // Sleep in loop
+    for (let i = 0; i < 50; i++) {
+        if (i === 3)
+            await sleep(2000);
+
+    }
+}
+
+demo();
 
 function contains(list, search) {
     for (var i in search) {
@@ -26,6 +61,15 @@ function contains(list, search) {
             if (x == search[i]) {
                 return true;
             }
+        }
+    }
+    return false;
+}
+
+function hasType(list, type) {
+    for (var x in list) {
+        if (x.startsWith(type)) {
+            return true;
         }
     }
     return false;
