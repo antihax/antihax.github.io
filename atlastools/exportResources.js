3d77207f3299f43152a4edd6fccb6826db7244d8
antihax <antihax@gmail.com>
2021-06-28 18:59:55 -0500

    update

--- atlastools/exportResources.js
+++ atlastools/exportResources.js
@@ -7,7 +7,9 @@ let islands = JSON.parse(rawdata);
 rawdata = fs.readFileSync('./json/resourceTypes.json');
 let resourceTypes = JSON.parse(rawdata);
 let types = {};
+let nodes = {};
 var resources = {};
+var nodecount = {};
 var animals = {};
 
 for (var island in islands) {
@@ -16,21 +18,29 @@ for (var island in islands) {
         animals[i.animals[animal]] = "";
     }
     for (var resource in i.resources) {
-        resource = resource.replace(/ \(Rock\)/g, '');
-        if (!resources[resource])
-            resources[resource] = {
+        let res = resource.replace(/ \(Rock\)/g, '');
+        if (!resources[res])
+            resources[res] = {
                 count: 0,
-                type: resourceTypes[resource]
+                type: resourceTypes[res]
             };
-        if (!resources[resource].type) {
-            console.log("Missing " + resource)
+        if (!nodecount[res])
+            nodecount[res] = {
+                count: 0,
+                type: resourceTypes[res]
+            };
+        if (!resources[res].type) {
+            console.log("Missing " + res)
         }
-        resources[resource].count++;
+
+        resources[res].count++;
+        nodecount[res].count += i.resources[resource];
     }
 }
 
 for (var type in resourceTypes) {
     types[resourceTypes[type]] = new Array();
+    nodes[resourceTypes[type]] = new Array();
 }
 
 for (var type in types) {
@@ -41,11 +51,19 @@ for (var type in types) {
             types[type].push(add);
         }
     }
+    for (var resource in nodecount) {
+        if (nodecount[resource].type === type) {
+            let add = {};
+            add[resource] = nodecount[resource].count;
+            nodes[type].push(add);
+        }
+    }
+
 }
-
+fs.writeFileSync('./json/nodeCount.json', JSON.stringify(nodes, null, "\t"));
 fs.writeFileSync('./json/animalCheck.json', JSON.stringify(animals, null, "\t"));
-
 fs.writeFileSync('./json/resourceCheck.json', JSON.stringify(types, null, "\t"));
+
 
 console.log("done")
 
