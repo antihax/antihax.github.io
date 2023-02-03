5d048affb771b9c465936f9facb0a74108f423ae
antihax <antihax@gmail.com>
2023-02-02 20:08:19 -0600

    add PvPv4

---
+++ json-pvpv4/config.js
@@ -0,0 +1,27 @@
+{
+	"AtlasMapServer": false,
+	"GPSBounds": {
+		"max": [
+			100,
+			-100
+		],
+		"min": [
+			-100,
+			100
+		]
+	},
+	"GridOffset": 23333.333333333332,
+	"GridSize": 1400000,
+	"ItemLink": true,
+	"KofiLink": true,
+	"NodesPerAxis": 30,
+	"PathFinder": true,
+	"PinTool": true,
+	"ServersX": 3,
+	"ServersY": 3,
+	"WarehouseTool": false,
+	"XRange": 200,
+	"XScale": 1,
+	"YRange": 200,
+	"YScale": 1
+}
