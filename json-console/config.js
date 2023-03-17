a55e9d20ac950072f2b3cf278329ea0fe154ba9e
antihax <antihax@gmail.com>
2023-03-17 15:03:45 -0500

    step 4

---
+++ json-console/config.js
@@ -0,0 +1,27 @@
+{
+	"GPSBounds": {
+		"max": [
+			100,
+			-100
+		],
+		"min": [
+			-100,
+			100
+		]
+	},
+	"GridOffset": 23333.333333333332,
+	"GridSize": 1400000,
+	"NodesPerAxis": 30,
+	"ServersX": 3,
+	"ServersY": 3,
+	"XRange": 200,
+	"XScale": 1,
+	"YRange": 200,
+	"YScale": 1,
+	"AtlasMapServer": false,
+	"ItemLink": true,
+	"KofiLink": true,
+	"WarehouseTool": true,
+	"PathFinder": true,
+	"PinTool": true
+}
